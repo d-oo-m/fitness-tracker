ac7603fe84d8e0a5574e1a151473171ac97ef9f8
Ilchuk Nazar <ilchuk_n@ki-technology.ru>
2018-02-14 16:44:38 +0200

    5.5 routing and authentication

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Subscription } from 'rxjs/Subscription';
-import {Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
 
 @Component({
@@ -22,6 +22,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   onToggleSidenav() {
     this.sidenavToggle.emit();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 
   ngOnDestroy() {
