08243be61c481d9d649d3e15c4298b61ba61677e
Ilchuk Nazar <ilchuk_n@ki-technology.ru>
2018-02-14 17:40:24 +0200

    5.7 route protection and preparing exercise data

---
+++ src/app/training/training.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Exercise } from './excercise.model';
+
+
+@Injectable()
+export class TrainingService {
+  availableExercises: Exercise[] = [
+    { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
+    { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
+    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
+    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
+  ];
+}
