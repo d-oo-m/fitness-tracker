dfbf5ea90d76378518818473f671c60b56873f8e
Ilchuk Nazar <ilchuk_n@ki-technology.ru>
2018-02-14 18:00:07 +0200

    5.8 injecting the training service

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -4,10 +4,14 @@ import { Exercise } from './excercise.model';
 
 @Injectable()
 export class TrainingService {
-  availableExercises: Exercise[] = [
+  private availableExercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
     { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
     { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
   ];
+
+  getAvailableExercises() {
+    return this.availableExercises.slice();
+  }
 }
