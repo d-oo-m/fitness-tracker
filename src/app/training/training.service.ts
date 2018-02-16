fcb4e39e69db17a419c14d50eb9801323a20dd0a
Ilchuk Nazar <ilchuk_n@ki-technology.ru>
2018-02-16 19:23:07 +0200

    Section5.13 progress

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,9 +1,13 @@
+import { Subject } from 'rxjs/Subject';
+
 import { Injectable } from '@angular/core';
 import { Exercise } from './excercise.model';
 
 
 @Injectable()
 export class TrainingService {
+  exerciseChanged = new Subject<Exercise>();
+
   private availableExercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
@@ -11,6 +15,7 @@ export class TrainingService {
     { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
   ];
   private runningExcercise: Exercise;
+  private exercises: Exercise[] = [];
 
   getAvailableExercises() {
     return this.availableExercises.slice();
@@ -18,5 +23,28 @@ export class TrainingService {
 
   startExercise(selectedId: string) {
     this.runningExcercise = this.availableExercises.find(ex => ex.id === selectedId);
+    this.exerciseChanged.next({ ...this.runningExcercise });
+  }
+
+  completeExercise() {
+    this.exercises.push( { ...this.runningExcercise, date: new Date(), state: 'completed' });
+    this.runningExcercise = null;
+    this.exerciseChanged.next(null);
+  }
+
+  cancelExercise(progress: number) {
+    this.exercises.push( {
+      ...this.runningExcercise,
+      duration: this.runningExcercise.duration * (progress / 100),
+      calories: this.runningExcercise.calories * (progress / 100),
+      date: new Date(),
+      state: 'cancelled'
+    });
+    this.runningExcercise = null;
+    this.exerciseChanged.next(null);
+  }
+
+  getRunningExercise() {
+    return { ...this.runningExcercise };
   }
 }
